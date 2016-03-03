8c1fe414a8830f15109d89bda1dc4d815976a0e5
Rebecca Edwards <rebecca.edwards@gmail.com>
2016-03-03 20:04:39 +0100

    Installing body-parser and temp code for testing addition of item to inventory

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded(({ extended: false })));
+app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
 	response.send('Hello from Express!');
+});
+
+let inventory = {};
+
+app.post('/', (request, response) => {
+	let item = request.body;
+	if (inventory[item.label] !== undefined) {
+		response.send('item already exists');
+	} else {
+		inventory[item.label] = item;
+		response.send(inventory);
+	}
 });
 
 app.listen(3000, () => {
